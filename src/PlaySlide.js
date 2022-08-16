f480667031347cc4c84a7b915377dfd835860cd2
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-08-16 18:58:44 -0500

    Add function to shuffle answers

--- src/PlaySlide.js
+++ src/PlaySlide.js
@@ -6,9 +6,28 @@ class PlaySlide extends React.Component {
     super(props);
     this.state = {
       shuffledAnswers: [],
-      correct: 0,
-      incorrect: 0
+      correctIndex: null,
+      correct: null,
+      incorrect: null
     }
+  }
+
+  shuffle = () => {
+    let array = [];
+    for (let i = 0; i < this.props.question.incorrect_answers.length; i++) array.push(i);
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    let shuffledAnswers = array.map(i => this.props.question.incorrect_answers[i]);
+    let insert = Math.floor(Math.random() * (array.length + 1))
+    shuffledAnswers.splice(insert,0,this.state.correct_answer);
+    this.setState({
+      shuffledAnswers: shuffledAnswers,
+      correctIndex: insert
+    })
   }
 
   render() {
