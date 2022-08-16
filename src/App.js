129b7feac3e2ca7b4481a9ec8a907e97f3882822
= <vapradeep817@gmail.com>
2022-08-16 10:03:58 -0500

    Add Auth0

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import './App.css';
+import { withAuth0 } from '@auth0/auth0-react';
 
 class App extends React.Component {
   render() {
+    const { user, isAuthenticated } = this.props.auth0;
     return (
       <div className="App">
         <p>Hello World!</p>
