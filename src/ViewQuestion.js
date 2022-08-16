4df724759f5c16eb1d7ea872bdcfce5d4265f1a3
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-08-16 15:23:31 -0500

    Add view quiz and questions components

---
+++ src/ViewQuestion.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+class ViewQuestion extends React.Component {
+  render() {
+    return (
+      <Card>
+        <Card.Title>{this.props.question.question}</Card.Title>
+        <Card.Text>Correct Answer: {this.props.question.correct}</Card.Text>
+        {this.props.question.incorrect.map((answer, index) => <Card.Text key={index}>Incorrect Answer: {answer}</Card.Text>)}
+      </Card>
+    )
+  }
+}
+
+export default ViewQuestion;
---
+++ src/ViewQuiz.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Button, Modal, Link } from 'react-bootstrap';
+import ViewQuestion from './ViewQuestion';
+
+class ViewQuiz extends React.Component {
+  handleDelete = (event) => {
+    this.props.deleteQuiz(this.props.quiz._id);
+  }
+  render() {
+    return(
+      <Modal show={this.props.showModal} onHide={this.props.hideModal} >
+        <Modal.Header closeButton>
+          <Modal.Title>{this.props.quiz.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {this.props.quiz.questions.map((question,index) => <ViewQuestion question={question} key={index} />)}
+          <div style={{display: 'flex', justifyContent: 'space-evenly'}}>
+            <Button variant='success'><Link to={`/play/${this.props.quiz._id}`}>Play</Link></Button>
+            <Button variant='primary'><Link to={`/update/${this.props.quiz._id}`}>Update</Link></Button>
+            <Button variant='danger' onClick={this.handleDelete}>Delete</Button>
+          </div>
+        </Modal.Body>
+      </Modal>
+    )
+  }
+}
+
+export default ViewQuiz;
