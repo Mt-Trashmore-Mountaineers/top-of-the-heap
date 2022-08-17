0e137f0788d185650900e2690acb48ae24d82435
= <vapradeep817@gmail.com>
2022-08-17 12:04:46 -0500

    Add about page and profile pictures

--- src/App.js
+++ src/App.js
@@ -3,21 +3,23 @@ import React from 'react';
 import './css/App.css';
 import './css/button.css';
 import { withAuth0 } from '@auth0/auth0-react';
-import QuizList from './QuizList';
+import QuizList from './QuizList.js';
+import About from './About.js';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
-    const { isAuthenticated, loginWithRedirect, logout } = this.props.auth0;
+    const { user, isAuthenticated, loginWithRedirect, logout } = this.props.auth0;
     return (
       <Router className="App">
         <nav className="navbar">
           <h2>Quiz App</h2>
           <input className="searchbar"></input>
           <div className="button-container">
-            <button className="primary">Create</button>
-            <button className="primary">Browse</button>
-            <button className="primary">Play</button>
+            <a href="/create" className="primary">Create</a>
+            <a href="/user" className="primary">Browse</a>
+            <a href="/play" className="primary">Play</a>
+            <a href="/about" className="primary">About</a>
             {
               // Log in/out button
               isAuthenticated ?
@@ -26,10 +28,14 @@ class App extends React.Component {
                 }}>Log out</button> :
                 <button className="primary" onClick={loginWithRedirect}>Login</button>
             }
+            <img id="profile-picture" alt="profile" src={user ? user.picture : "nothing here"}/>
           </div>
         </nav>
         <Routes>
           <Route path="user" element={<QuizList />} ></Route>
+        </Routes>
+        <Routes>
+          <Route path="about" element={<About />} ></Route>
         </Routes>
       </Router>
     )
